b58d75d9a8b271952c7ea0f41d62dcf3117c10af
Martin Pettersson <martin_pettersson@outlook.com>
2016-09-01 14:27:30 +0200

    Added a resource response interface to enable a controller to be available cross SAPI

---
+++ src/Kernel/ResourceResponseInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Copyright (c) 2016 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Solid\Kernel;
+
+/**
+ * @package Solid\Kernel
+ * @author Martin Pettersson <martin@solid-framework.com>
+ * @since 0.1.0
+ */
+interface ResourceResponseInterface
+{
+    /**
+     * @api
+     * @since 0.1.0
+     * @return string
+     */
+    public function __toString(): string;
+}
